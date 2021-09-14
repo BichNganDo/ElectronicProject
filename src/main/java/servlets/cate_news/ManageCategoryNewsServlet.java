a893919df98846e93ce9c3cf842fea10b245d216
Ngan Do <ngando6598@gmail.com>
2021-09-14 20:04:18 +0700

    toi gian phan header, footer cua cac trang html

--- src/main/java/servlets/cate_news/ManageCategoryNewsServlet.java
+++ src/main/java/servlets/cate_news/ManageCategoryNewsServlet.java
@@ -18,6 +18,11 @@ public class ManageCategoryNewsServlet extends HttpServlet {
         pageVariables.put("app_domain", Config.APP_DOMAIN);
         pageVariables.put("static_domain", Config.STATIC_DOMAIN);
 
+        Map<String, Object> pageVariablesHeader = new HashMap<>();
+        pageVariablesHeader.put("static_domain", Config.STATIC_DOMAIN);
+        pageVariables.put("header_include", PageGenerator.instance().getPage("include/header.html", pageVariablesHeader));
+        pageVariables.put("footer_include", PageGenerator.instance().getPage("include/footer.html", pageVariablesHeader));
+
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().println(PageGenerator.instance().getPage("category_news/manage_cate_news.html", pageVariables));
 
--- src/main/java/servlets/product/AddProductServlet.java
+++ src/main/java/servlets/product/AddProductServlet.java
@@ -18,6 +18,11 @@ public class AddProductServlet extends HttpServlet {
         pageVariables.put("app_domain", Config.APP_DOMAIN);
         pageVariables.put("static_domain", Config.STATIC_DOMAIN);
 
+        Map<String, Object> pageVariablesHeader = new HashMap<>();
+        pageVariablesHeader.put("static_domain", Config.STATIC_DOMAIN);
+        pageVariables.put("header_include", PageGenerator.instance().getPage("include/header.html", pageVariablesHeader));
+        pageVariables.put("footer_include", PageGenerator.instance().getPage("include/footer.html", pageVariablesHeader));
+
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().println(PageGenerator.instance().getPage("product/add_product.html", pageVariables));
 
